aa06aa55f7d13f0cd730d80e6cf095dc98ca85a0
sahilali13 <ali.sahil.a743@gmail.com>
2024-03-14 15:26:48 +0530

    moved EDP to enterprise-resouce-planning repo

--- ERM/src/helpers/GenerateUniqueId.jsx
+++
@@ -1,5 +0,0 @@
-export default function GenerateUniqueId() {
-	const timestamp = Date.now().toString(36);
-	const randomString = Math.random().toString(36).substring(2);
-	return timestamp + randomString;
-}
